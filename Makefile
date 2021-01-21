d665cb2811c5f0a875c968cfaa34da744497db1e
Tomoyuki Hata <hato6502@gmail.com>
2021-01-22 03:23:14 +0900

    Add vcd snapshot

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 index-test: src/index.v src/index.test.v
-  iverilog -o bin/index-test src/index.v src/index.test.v
+	iverilog -o bin/index-test src/index.v src/index.test.v
--- src/index.v
+++ src/index.v
@@ -1,2 +1,7 @@
-module index();
+module index(input wire in_1,
+             input wire in_2,
+             output wire out);
+    
+    assign out = in_1 & in_2;
+    
 endmodule
