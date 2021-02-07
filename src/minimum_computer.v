d0e82075c2304d963bbf3f95179edf431f290a9e
Tomoyuki Hata <hato6502@gmail.com>
2021-02-08 01:30:35 +0900

    Implement NTSC generator

--- src/minimum_computer.v
+++ src/minimum_computer.v
@@ -1,37 +1,46 @@
 module minimum_computer(input wire clock,
-                        output reg [10:0] clock_counter);
+                        output wire luminance,
+                        output wire sync);
+    wire is_horizontal_area;
+    wire is_vertical_area;
+    
+    wire horizontal_sync;
+    wire vertical_sync;
+    
+    reg [10:0] clock_counter;
+    reg [8:0] line_counter;
+    
+    assign is_horizontal_area = clock_counter >= 512 & clock_counter < (1536 + 8*8*4);
+    assign is_vertical_area   = line_counter >= 32 & line_counter<232;
+    assign luminance          = is_horizontal_area & is_vertical_area;
+    
+    assign horizontal_sync = clock_counter < 150;
+    assign vertical_sync   = line_counter < 3;
+    assign sync            = ~(horizontal_sync^vertical_sync);
     
     initial
     begin
         clock_counter = 0;
+        line_counter  = 0;
     end
     
     always @ (posedge clock)
     begin
-        clock_counter <= clock_counter + 1;
+        if (clock_counter < 2031) begin
+            clock_counter <= clock_counter + 1;
+        end
+        else begin
+            if (line_counter < 261) begin
+                line_counter <= line_counter + 1;
+            end
+            else begin
+                line_counter <= 0;
+            end
+            
+            clock_counter <= 0;
+        end
     end
     /*
-     process(clk) begin
-     if (rising_edge(clk)) then
-     case ppu_clkcnt is
-     when 2032 = >
-     ppu_clkcnt <= 0;
-     
-     case ppu_linecnt is
-     when 262 = >
-     ppu_linecnt <= 0;
-     when others = >
-     ppu_linecnt <= ppu_linecnt + 1;
-     end case;
-     
-     when others = >
-     ppu_clkcnt <= ppu_clkcnt + 1;
-     end case;
-     end if;
-     end process;
-     
-     stat_vsync <= '1' when ppu_linecnt<3 else '0';
-     stat_hsync <= '1' when ppu_clkcnt<150 else '0';
      
      lum <= crom_1_dot;
      */
--- src/minimum_computer_test.v
+++ src/minimum_computer_test.v
@@ -1,14 +1,78 @@
 /* verilator lint_off STMTDLY */
 
-module minimum_computer_test(output wire [10:0] clock_counter);
-    reg clock; minimum_computer minimum_computer(clock,
-    clock_counter);
+module minimum_computer_test(output wire luminance,
+                             output wire sync);
+    reg clock;
+    
+    minimum_computer minimum_computer(clock, luminance, sync);
     
     initial begin
-        $dumpfile("src/minimum_computer_test.vcd");
-        $dumpvars(0);
+        clock = 0;
         
-        repeat(4096) begin
+        repeat(2032*31) begin
+            #10;
+            clock = 1;
+            
+            #10;
+            clock = 0;
+        end
+        
+        $dumpfile("src/minimum_computer_test.vcd");
+        $dumpvars(1);
+        
+        // Vertical drawing area start
+        repeat(2032*2) begin
+            #10;
+            clock = 1;
+            
+            #10;
+            clock = 0;
+        end
+        
+        // drawing line
+        repeat(2032*1) begin
+            #10;
+            clock = 1;
+            
+            #10;
+            clock = 0;
+        end
+        
+        $dumpoff();
+        
+        repeat(2032*197) begin
+            #10;
+            clock = 1;
+            
+            #10;
+            clock = 0;
+        end
+        
+        $dumpon();
+        
+        // Vertical drawing area end
+        repeat(2032*2) begin
+            #10;
+            clock = 1;
+            
+            #10;
+            clock = 0;
+        end
+        
+        $dumpoff();
+        
+        repeat(2032*28) begin
+            #10;
+            clock = 1;
+            
+            #10;
+            clock = 0;
+        end
+        
+        $dumpon();
+        
+        // Vertical sync
+        repeat(2032*5) begin
             #10;
             clock = 1;
             
@@ -19,5 +83,4 @@ module minimum_computer_test(output wire [10:0] clock_counter);
         #10;
         $finish;
     end
-    
 endmodule
