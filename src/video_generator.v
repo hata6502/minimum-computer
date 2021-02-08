be5ec2c2d41b492e1015fa42c6294be52b2fca4e
Tomoyuki Hata <hato6502@gmail.com>
2021-02-08 23:37:39 +0900

    Use verible-verilog-format

--- src/video_generator.v
+++ src/video_generator.v
@@ -1,48 +1,46 @@
-module video_generator(input wire clock,
-                        output wire luminance,
-                        output wire sync);
-    wire is_horizontal_area;
-    wire is_vertical_area;
-    
-    wire horizontal_sync;
-    wire vertical_sync;
-    
-    reg [10:0] clock_counter;
-    reg [8:0] line_counter;
-    
-    assign is_horizontal_area = clock_counter >= 512 & clock_counter < (1536 + 8*8*4);
-    assign is_vertical_area   = line_counter >= 32 & line_counter<232;
-    assign luminance          = is_horizontal_area & is_vertical_area;
-    
-    assign horizontal_sync = clock_counter < 150;
-    assign vertical_sync   = line_counter < 3;
-    assign sync            = ~(horizontal_sync^vertical_sync);
-    
-    initial
-    begin
-        clock_counter = 0;
-        line_counter  = 0;
+module video_generator (
+    input  wire clock,
+    output wire luminance,
+    output wire sync
+);
+  wire is_horizontal_area;
+  wire is_vertical_area;
+
+  wire horizontal_sync;
+  wire vertical_sync;
+
+  reg [10:0] clock_counter;
+  reg [8:0] line_counter;
+
+  assign is_horizontal_area = clock_counter >= 512 & clock_counter < (1536 + 8 * 8 * 4);
+  assign is_vertical_area   = line_counter >= 32 & line_counter < 232;
+  assign luminance          = is_horizontal_area & is_vertical_area;
+
+  assign horizontal_sync    = clock_counter < 150;
+  assign vertical_sync      = line_counter < 3;
+  assign sync               = ~(horizontal_sync ^ vertical_sync);
+
+  initial begin
+    clock_counter = 0;
+    line_counter  = 0;
+  end
+
+  always @(posedge clock) begin
+    if (clock_counter < 2031) begin
+      clock_counter <= clock_counter + 1;
+    end else begin
+      if (line_counter < 261) begin
+        line_counter <= line_counter + 1;
+      end else begin
+        line_counter <= 0;
+      end
+
+      clock_counter <= 0;
     end
-    
-    always @ (posedge clock)
-    begin
-        if (clock_counter < 2031) begin
-            clock_counter <= clock_counter + 1;
-        end
-        else begin
-            if (line_counter < 261) begin
-                line_counter <= line_counter + 1;
-            end
-            else begin
-                line_counter <= 0;
-            end
-            
-            clock_counter <= 0;
-        end
-    end
-    /*
+  end
+  /*
      
      lum <= crom_1_dot;
      */
-    
+
 endmodule
--- src/video_generator_test.v
+++ src/video_generator_test.v
@@ -1,86 +1,92 @@
 /* verilator lint_off STMTDLY */
 
-module video_generator_test(output wire luminance,
-                             output wire sync);
-    reg clock;
-    
-    video_generator video_generator(clock, luminance, sync);
-    
-    initial begin
-        clock = 0;
-        
-        repeat(2032*31) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        $dumpfile("src/video_generator_test.vcd");
-        $dumpvars(1);
-        
-        // Vertical drawing area start
-        repeat(2032*2) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        // drawing line
-        repeat(2032*1) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        $dumpoff();
-        
-        repeat(2032*197) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        $dumpon();
-        
-        // Vertical drawing area end
-        repeat(2032*2) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        $dumpoff();
-        
-        repeat(2032*28) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        $dumpon();
-        
-        // Vertical sync
-        repeat(2032*5) begin
-            #10;
-            clock = 1;
-            
-            #10;
-            clock = 0;
-        end
-        
-        #10;
-        $finish;
+module video_generator_test (
+    output wire luminance,
+    output wire sync
+);
+  reg clock;
+
+  video_generator video_generator (
+      clock,
+      luminance,
+      sync
+  );
+
+  initial begin
+    clock = 0;
+
+    repeat (2032 * 31) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
     end
+
+    $dumpfile("src/video_generator_test.vcd");
+    $dumpvars(1);
+
+    // Vertical drawing area start
+    repeat (2032 * 2) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
+    end
+
+    // drawing line
+    repeat (2032 * 1) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
+    end
+
+    $dumpoff();
+
+    repeat (2032 * 197) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
+    end
+
+    $dumpon();
+
+    // Vertical drawing area end
+    repeat (2032 * 2) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
+    end
+
+    $dumpoff();
+
+    repeat (2032 * 28) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
+    end
+
+    $dumpon();
+
+    // Vertical sync
+    repeat (2032 * 5) begin
+      #10;
+      clock = 1;
+
+      #10;
+      clock = 0;
+    end
+
+    #10;
+    $finish;
+  end
 endmodule
